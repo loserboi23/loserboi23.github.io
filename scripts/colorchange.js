04a83d2d59836feea5e86f62d52d5575a2815bfd
loserboi23 <fierysnake1@gmail.com>
2019-07-11 20:17:45 -0700

    scripts v1

---
+++ scripts/colorchange.js
@@ -0,0 +1,5 @@
+var styles = getComputedStyle(document.documentElement);
+var secondary = styles.getPropertyValue('--secondary');
+//document.documentElement.style.setProperty('--primary',secondary);
+
+alert(window.location.pathname);
